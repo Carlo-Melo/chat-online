903079e03e6715d61235da75365d3a6799cef36c
Carlos Eduardo Melo <133800041+Carlo-Melo@users.noreply.github.com>
2024-06-26 11:12:08 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://chat-frontend-g2yh.onrender.com/")
+    websocket = new WebSocket("https://chat-frontend-g2yh.onrender.com/")
     websocket.onmessage = processMessage
 }
 
